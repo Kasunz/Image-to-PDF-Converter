585859c9986fdfa14fdd10e03791d6413a462dfc
Kasun-Madusanka-Bandara <kasunmadhusankayt@gmail.com>
2024-10-07 00:11:46 +0530

    GUI part is done
    after this lets begin buttons functions

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 
 class ImageToPDFConverter:
     def __init__(self, root):
+        self.select_images = None
+        self.convert_images_to_pdf = None
         self.root = root
         self.image_path = []
         self.output_pdf_name = tk.StringVar()
@@ -12,12 +14,27 @@ class ImageToPDFConverter:
         self.initialize_ui()
 
     def initialize_ui(self):
-        title_label = tk.Label(self.root, text="Image To PDF Convertor", font=("Helvetica", 16, "Bold"))
+        title_label = tk.Label(self.root, text="Image To PDF Convertor", font=("Helvetica", 16, "bold"))
         title_label.pack(pady=10)
+
+        select_images_button = tk.Button(self.root, text="Select Images", command=self.select_images)
+        select_images_button.pack(pady=(0, 10))
+
+        self.selected_images_listbox.pack(pady=(0, 10), fill=tk.BOTH, expand=True)
+
+        label = tk.Label(self.root, text="Enter output PDF name")
+        label.pack()
+
+        pdf_name_entry = tk.Entry(self.root, textvariable=self.output_pdf_name, width=40)
+        pdf_name_entry.pack()
+
+        convert_button = tk.Button(self.root, text="Convert to PDF", command=self.convert_images_to_pdf)
+        convert_button.pack(pady=(20, 40))
 
 def main():
     root = tk.Tk()
     root.title("Image to PDF")
+    converter = ImageToPDFConverter(root)
     root.geometry("400x600")
     root.mainloop()
 
